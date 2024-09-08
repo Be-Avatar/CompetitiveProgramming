8907c23237568e771ee615036fc65334f4596726
BEIMNET BAHIRU <beimnet602@gmail.com>
2024-09-08 21:44:38 +0300

    Time: 32 ms (74.38%), Space: 16.6 MB (19.15%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.py
+++ 2469-convert-the-temperature/2469-convert-the-temperature.py
@@ -1,3 +1,6 @@
 class Solution:
     def convertTemperature(self, celsius: float) -> List[float]:
-        return [celsius + 27315/100, celsius * 9/5 + 320/10]
+        arr = []  
+        arr.append(celsius + 5463/20)  
+        arr.append(celsius * 9/5 + 32)  
+        return arr
