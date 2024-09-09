c517b08194b4e9f048111cc75672bfa4a3f19c07
BEIMNET BAHIRU <beimnet602@gmail.com>
2024-09-09 08:20:53 +0300

    Time: 39 ms (78.32%), Space: 17.6 MB (46.26%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,15 +1,15 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        stk = []
-        for i in range(1, n+1):
-            stk.append(str(i))
-        if n >= 3:
-            for j in range(2, n, 3):
-                stk[j] = "Fizz"
-        if n >= 5:
-            for k in range(4, n, 5):
-                stk[k] = "Buzz"
-        if n >= 15:
-            for l in range(14, n, 15):
-                stk[l] = "FizzBuzz"
-        return stk
+        res = ['']*n
+        for i in range(0,n,3):
+            res[i]=str(i+1)
+        for i in range(1,n,3):
+            res[i]=str(i+1)
+        for i in range(2,n,3):
+            res[i]="Fizz"
+        for i in range(4,n,5):
+            if res[i]=="Fizz":
+                res[i]+="Buzz"
+            else:
+                res[i]="Buzz"
+        return res
