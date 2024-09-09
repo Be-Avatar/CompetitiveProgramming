3ea0af46246e2794a932cba96bdba6f47e25fc8d
BEIMNET BAHIRU <beimnet602@gmail.com>
2024-09-09 08:14:48 +0300

    Time: 44 ms (47.90%), Space: 17.6 MB (89.02%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,13 +1,15 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        stack = []
-        for i in range(1, n + 1):
-            if i % 3 == 0 and i % 5 == 0:
-                stack.append('FizzBuzz')
-            elif i % 3 == 0:
-                stack.append('Fizz')
-            elif i % 5 == 0:
-                stack.append('Buzz')
-            else:
-                stack.append(str(i))
-        return stack
+        stk = []
+        for i in range(1, n+1):
+            stk.append(str(i))
+        if n >= 3:
+            for j in range(2, n, 3):
+                stk[j] = "Fizz"
+        if n >= 5:
+            for k in range(4, n, 5):
+                stk[k] = "Buzz"
+        if n >= 15:
+            for l in range(14, n, 15):
+                stk[l] = "FizzBuzz"
+        return stk
