25690eea54d7acbde127bddf4108795217dbd843
BEIMNET BAHIRU <beimnet602@gmail.com>
2024-09-14 22:12:11 +0300

    Time: 55 ms (53.79%), Space: 16.8 MB (52.10%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,3 +1,7 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        return list(set(permutations(nums)))
+        perm_set = set(permutations(nums))
+        ans = []
+        for i in perm_set:
+            ans.append(i)
+        return ans
