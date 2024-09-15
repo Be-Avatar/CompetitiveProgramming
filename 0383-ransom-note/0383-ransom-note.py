6d49697d29bad30bd7e3ca1b572fa4c143aae06c
BEIMNET BAHIRU <beimnet602@gmail.com>
2024-09-15 18:25:43 +0300

    Time: 41 ms (93.70%), Space: 16.8 MB (7.32%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,21 +1,9 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ran_dic = {}
-        mag_dic = {}
-        
-        for i in ransomNote:
-            if i in ran_dic:
-                ran_dic[i] += 1
-            else:
-                ran_dic[i] = 1
-                
-        for j in magazine:
-            if j in mag_dic:
-                mag_dic[j] += 1
-            else:
-                mag_dic[j] = 1
-                
-        boolean = True
-        for k in set(ransomNote):
-            boolean = boolean and ran_dic[k] <= mag_dic.get(k, 0)
-        return boolean
+        if len(ransomNote) > len(magazine):
+            return False
+            
+        for c in set(ransomNote):
+            if magazine.count(c) < ransomNote.count(c):
+                return False
+        return True
